442d43ae22dad87be58f3e721e230d363b7aa95f
Henrik D. Flemmen <hflemmen@gmail.com>
2020-01-21 00:04:48 +0100

    Minor changes

--- react_agent/scripts/agent.py
+++ react_agent/scripts/agent.py
@@ -5,12 +5,20 @@
 # this was copied from https://stackoverflow.com/questions/48830056/use-data-from-multiple-topics-in-ros-python
 import rospy
 from std_msgs.msg import Float64MultiArray
+from teraranger_array.msg import RangeArray
+from human_info.msg import HumanPos
+import decision_tree
 
 
 class Server:
     def __init__(self):
-        self.orientation = None
-        self.velocity = None
+        self.human_dir = None
+        self.human_dist = None
+        self.distances = []
+
+        self.ai = decision_tree.Decision_tree()
+
+        #TODO: Make publisher here
 
     def orientation_callback(self, msg):
         # "Store" message received.
@@ -19,16 +27,19 @@ class Server:
         # Compute stuff.
         self.compute_stuff()
 
-    def velocity_callback(self, msg):
+    def ranges_callback(self, msg):
         # "Store" the message received.
-        self.velocity = msg
+        # TODO: Check what kind of format the data becomes in python.
+        self.distances = msg
+        print("Terraranger array messages:", msg)
 
         # Compute stuff.
         self.compute_stuff()
 
     def compute_stuff(self):
         if self.orientation is not None and self.velocity is not None:
-            pass  # Compute something.
+            res = self.ai.update()
+
 
 
 if __name__ == '__main__':
@@ -36,7 +47,7 @@ if __name__ == '__main__':
 
     server = Server()
 
-    rospy.Subscriber('/orientation', Float64MultiArray , server.orientation_callback)
-    rospy.Subscriber('/velocity', Float64MultiArray, server.velocity_callback)
+    rospy.Subscriber('/human_info/human_info', Float64MultiArray, server.orientation_callback)
+    rospy.Subscriber('/multiflex_1/ranges_raw', RangeArray, server.ranges_callback)
 
     rospy.spin()
--- react_agent/scripts/decision_tree.py
+++ react_agent/scripts/decision_tree.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
-class: Decision_tree:
+class Decision_tree:
 	def __innit__(self):
 		pass
 	
 	def update(distances, where_is_human, how_far_is_human, ): # this takes all inputs from all sensors etc.
 		state_var = 0
-		if (state_var) = 0:
-			if where_is_human = NONE || how_far_is_human = NONE:
+		if (state_var) == 0:
+			if where_is_human == None || how_far_is_human == None:
 				state_var = 1
 			if d_sensor[] < d_threshold:
 				state_var = 2
