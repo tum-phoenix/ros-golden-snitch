1e69b7108a6088aec5fc22321730a24ebfd19504
Henrik D. Flemmen <hflemmen@gmail.com>
2020-01-21 19:25:01 +0100

    minor changes

--- react_agent/scripts/decision_tree.py
+++ react_agent/scripts/decision_tree.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
 class Decision_tree:
-	def __innit__(self):
+	def __init__(self):
 		pass
 
 	# @returns : a python list of length 4 on the format : [pox_x, pos_y, pos_z, yaw] in the local frame
-	def update(self, distances, where_is_human, how_far_is_human, ): # this takes all inputs from all sensors etc.
+	def update(self, distances, where_is_human, how_far_is_human, ):  # this takes all inputs from all sensors etc.
 		state_var = 0
 		d_threshold = 1
 		if (state_var) == 0:
@@ -13,7 +13,6 @@ class Decision_tree:
 				state_var = 1
 			if distances[0] < d_threshold:
 				state_var = 2
-		 
 		elif(state_var) == 1:
 			pass
 		elif(state_var) == 2:
