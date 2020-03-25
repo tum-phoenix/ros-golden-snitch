32bb21f75aea13d2d0c701af3deb2d871c4e71bc
Henrik D. Flemmen <hflemmen@gmail.com>
2020-03-25 19:42:06 +0100

    Added conditional verbosity level in the mavros interface.

--- phx_launch/scripts/mavrosInterface.py
+++ phx_launch/scripts/mavrosInterface.py
@@ -41,11 +41,12 @@ class MavrosUAV:
         self.land_srv = rospy.ServiceProxy(landing_srv_name, CommandTOL)
 
 
-    def init_uav(self, arm=False, takeoff=False):
-        print("Starting init_uav(). Atm we have to manualy accept every new command, but just remove the `input()` from the code to do this faster.")
+    def init_uav(self, arm=False, takeoff=False, verbose=False):
+        if verbose:
+            print("Starting init_uav(). Atm we have to manualy accept every new command, but just remove the `input()` from the code to do this faster.")
         res_setMode = self.setmode_srv(0,'GUIDED')
-        print("Res of setmode:", res_setMode)
-        input()
+        if verbose:
+            print("Res of setmode:", res_setMode)
 
 
         origin = GeoPointStamped()
@@ -54,8 +55,9 @@ class MavrosUAV:
         origin.position.latitude = OPERATION_POSITION[0]
         origin.position.longitude = OPERATION_POSITION[1]
         origin.position.altitude = OPERATION_POSITION[2]
-        self.set_origin_pub.publish(origin)
-        print("Published ",origin)
+        self.set_origin_pub.publish(origin) # TODO: It might be that we need to wait after sending this, but I don't know for what.
+        if verbose:
+            print("Published ",origin)
 
         if arm or takeoff:
             self.arm()
@@ -63,25 +65,26 @@ class MavrosUAV:
                 self.takeoff()
 
 
-    def arm(self):
+    def arm(self, verbose=False):
         res_arm = self.arm_srv(True)
-        print("Result of arming:", res_arm)
-        input()
+        if verbose:
+            print("Result of arming:", res_arm)
 
     # block: True if the call should block until we have reached the specified altitude.
-    def takeoff(self, altitude=1, block=True):
+    def takeoff(self, altitude=1, block=True, verbose=False):
         res_takeoff = self.takeoff_srv(min_pitch=0.0, yaw=0.0,latitude=OPERATION_POSITION[0], longitude=OPERATION_POSITION[1], altitude=altitude)
-        print("Result of takeoff", res_takeoff)
+        if verbose:
+            print("Result of takeoff", res_takeoff)
         if block:
             time.sleep(5) # TODO: Replace this with a check for the height and see that it is >=0.9*altitude.
 
     # block is True if the call should block until the UAV is landed.
-    def land(self, block=True):
+    def land(self, block=True, verbose=False):
         res_land = self.land_srv(min_pitch=0.0, yaw=0.0,latitude=OPERATION_POSITION[0], longitude=OPERATION_POSITION[1], altitude=1)
-        print("Result of landing", res_land)
+        if verbose:
+            print("Result of landing", res_land)
 
     # This is in the MAV frame.
     def set_vel_setpoint(self, dir, rot):
         new_vel = Twist(Vector3(*dir), Vector3(*rot))  # TODO: Should this be StapmedTwist instead?
         self.set_vel_pub.publish(new_vel)
-        input("Just sent command to move")
