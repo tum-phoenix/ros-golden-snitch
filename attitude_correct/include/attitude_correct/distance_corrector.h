539d0273fc6e7f05ea357b2a62c0be1b76d3ecd7
Henrik D. Flemmen <hflemmen@gmail.com>
2020-04-16 18:46:31 +0200

    Presise tests are added.

--- attitude_correct/include/attitude_correct/distance_corrector.h
+++ attitude_correct/include/attitude_correct/distance_corrector.h
@@ -13,7 +13,7 @@ const unsigned int numOfRangeSensors = 8;
 /*
  * This function assumes level, flat floor, vertical walls and no other objects that does not conform to these assumptions.
  *  (It can still probably be used, but will give wrong results)
- * @param: attitude assumes for now that it is the roll, pitch, yaw euler angles in that order.
+ * @param: (radians) attitude assumes for now that it is the roll, pitch, yaw euler angles in that order.
  * @param: dirOfRangeSensors contains the direction of the range sensors in degrees, where 0 is straight forward and increasing with direction of the clock.
  */
 void correctDistances(
--- attitude_correct/test/distance_corrector_test.cpp
+++ attitude_correct/test/distance_corrector_test.cpp
@@ -6,42 +6,83 @@
 #include "attitude_correct/distance_corrector.h"
 #include "gtest/gtest.h"
 #include <cmath>
+#include <algorithm>
 
 // TODO: Check what happens when the range sensor reading is out of range.
 
-class DistaneCorrectorTest : public testing::Test {
+class DistanceCorrectorTest : public testing::Test {
 protected:
     std::array<double, numOfRangeSensors> ranges;
     std::array<double, 3> attitude;
     double altitude;
     std::array<double, numOfRangeSensors> dirOfRangeSensors;
+    std::array<bool, numOfRangeSensors> noneDetected;
 
 
     virtual void SetUp() {
-        ranges = {1,2, 3, 3, 3, 1, 0.5, 0.8};
+        ranges = {1, 2, 3, 3, 3, 1, 0.5, 0.8};
         attitude = {0, 0, 0};
-        altitude = 1;
-        dirOfRangeSensors = {0, 45, 90, };
+        altitude = 5;
         dirOfRangeSensors = {0, M_PI/4, M_PI/2, 3*M_PI/4, M_PI, 5*M_PI/4, 3*M_PI/2, 7*M_PI/4};
+        noneDetected = {false, false, false, false, false, false, false, false};
     }
 };
 
-TEST_F(DistaneCorrectorTest, OneIsOne) {
-    EXPECT_EQ(1, 1);
+TEST_F(DistanceCorrectorTest, OneIsOne) {
+    ASSERT_EQ(1, 1);
 }
 
-TEST_F(DistaneCorrectorTest, NumOfRangeSensorsSet) {
-    EXPECT_GE(numOfRangeSensors,0);
+TEST_F(DistanceCorrectorTest, NumOfRangeSensorsSet) {
+    ASSERT_GT(numOfRangeSensors,0);
 }
 
-TEST_F(DistaneCorrectorTest, SimpleTests) {
+TEST_F(DistanceCorrectorTest, SimpleTests) {
     std::array<double, numOfRangeSensors> correctRanges{};
     std::array<bool, numOfRangeSensors> seesFloor{};
 
     correctDistances(ranges, attitude, altitude,dirOfRangeSensors,correctRanges,seesFloor);
-    EXPECT_EQ(ranges, correctRanges);
+    ASSERT_EQ(ranges, correctRanges);
+    ASSERT_EQ(seesFloor, noneDetected);
 
+    attitude = {0, 0, M_PI/2};
+    correctDistances(ranges, attitude, altitude,dirOfRangeSensors,correctRanges,seesFloor);
+    ASSERT_EQ(ranges, correctRanges);
+    ASSERT_EQ(seesFloor, noneDetected);
+
+
+    std::array<double, numOfRangeSensors> refRanges = correctRanges;
+    attitude = {M_PI/18, 0, 0};
+    correctDistances(ranges, attitude, altitude,dirOfRangeSensors,correctRanges,seesFloor);
+    ASSERT_GE(refRanges, correctRanges);
+    for (int i = 0; i<numOfRangeSensors;i++){
+        if (dirOfRangeSensors[i] !=  0 && dirOfRangeSensors[i] != M_PI){
+            ASSERT_GT(refRanges[i], correctRanges[i]);
+        }else{
+            ASSERT_EQ(refRanges[i], correctRanges[i]);
+        }
+    }
+
+    attitude = {0, M_PI/3, 0};
+    altitude = 1;
+    ranges[0] = 2.5;
+    correctDistances(ranges, attitude, altitude,dirOfRangeSensors,correctRanges,seesFloor);
+    ASSERT_EQ(true, seesFloor[0]);
 }
+
+TEST_F(DistanceCorrectorTest, PreciseTests){
+    std::array<double, numOfRangeSensors> correctRanges{};
+    std::array<bool, numOfRangeSensors> seesFloor{};
+
+    attitude = {0, M_PI/4, 0};
+    correctDistances(ranges, attitude, altitude,dirOfRangeSensors,correctRanges,seesFloor);
+    ASSERT_EQ(cos(M_PI/4) * ranges[0], correctRanges[0]);
+
+    attitude = {0, M_PI/4, M_PI/2};
+    correctDistances(ranges, attitude, altitude,dirOfRangeSensors,correctRanges,seesFloor);
+    ASSERT_EQ(cos(M_PI/4) * ranges[2], correctRanges[2]);
+}
+
+
 
 int main(int argc, char** argv){
     testing::InitGoogleTest(&argc, argv);
