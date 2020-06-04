5386e9a5e4dc40368d1e2a0332bd5707d4368f03
Henrik D. Flemmen <hflemmen@gmail.com>
2020-06-04 16:34:49 +0200

    Changes to the interfaces in local mapper.

--- local_map/nodes/map_node.py
+++ local_map/nodes/map_node.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+import rospkg
 import rospy
 import numpy as np
+import yaml
 from local_map.map_logic import Mapper
 from geometry_msgs.msg import PoseStamped
 from sensor_msgs.msg import PointCloud
@@ -9,14 +11,18 @@ from teraranger_array.msg import RangeArray
 
 class Server:
     def __init__(self):
-        self.pose = PoseStamped()
-        self.mapper = Mapper()
+
+        rospack = rospkg.RosPack()
+        with open(rospack.get_path("phx_launch")+"/../config/hardware_config.yaml") as f:
+            mechanical_config = yaml.load(f, Loader=yaml.SafeLoader)
+        dirOfRangeSensors = mechanical_config["direction_of_range_sensors"]
+        self.mapper = Mapper(dirOfRangeSensors)
         self.pub = rospy.Publisher("phx/local_map", PointCloud, queue_size=1)
 
 
     def ranges_callback(self, msg):
         ranges = np.array(list(map(lambda x: x.range, msg.ranges)))
-        map = self.mapper.update(ranges, self.pose)
+        map = self.mapper.update(ranges, self.pos, self.rot)
 
         # TODO Generate the correct point cloud message
         msg = PointCloud(map)
@@ -24,7 +30,8 @@ class Server:
 
     def _pose_callback(self, msg: PoseStamped):
         # TODO: Convert the pose to the desired format
-        self.pose = msg
+        self.pos = msg.pose.position
+        self.rot = [msg.pose.orientation.w, msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z]
 
 
 if __name__ == '__main__':
--- local_map/src/local_map/map_logic.py
+++ local_map/src/local_map/map_logic.py
@@ -4,13 +4,20 @@ from rospy import loginfo
 
 
 class Mapper:
-    def __init__(self):
+    def __init__(self, dirOfRangeSensosr):
         # TODO: Decide for feasible map datastructure
         self.map = []
+        self.dirOfRangeSensors = dirOfRangeSensosr
 
-    def update(self, ranges : np.ndarray, pose):
-        map = _update_map(self.map, ranges, pose)
 
-def _update_map(map, ranges, pose):
+    def update(self, ranges : np.ndarray, position, orientation):
+        """
+        @param position: 3 element list x, y, z
+        @param orientation: 4 element unit quaternion with w first: [w, x, y, x]
+        """
+        self.map = _update_map(self.map,self.dirOfRangeSensors, ranges, position, orientation)
+        return self.map
+
+def _update_map(map,dirOfRangeSensors, ranges, position, orientation):
     # TODO: Create the logic
-    pass
+    return map
