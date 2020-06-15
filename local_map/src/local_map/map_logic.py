cff99cb1e38abc770fb3c27f0aee7f17e39307df
Adrien Sorin <adrien.sorin@tum.de>
2020-06-16 01:29:07 +0200

    Main structure of the logic for update map is laid out, some points still have to be completed.

--- local_map/src/local_map/map_logic.py
+++ local_map/src/local_map/map_logic.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 import numpy as np
 from rospy import loginfo
-
+from math import cos,sin,tan
 
 class Mapper:
-    def __init__(self, dirOfRangeSensosr):
-        # TODO: Decide for feasible map datastructure
+    def __init__(self, dirOfRangeSensors):
+        # TODO: Decide for feasible map datastructure --> list of points
         self.map = []
-        self.dirOfRangeSensors = dirOfRangeSensosr
+        self.dirOfRangeSensors = dirOfRangeSensors
 
 
     def update(self, ranges : np.ndarray, position, orientation):
@@ -19,5 +19,39 @@ class Mapper:
         return self.map
 
 def _update_map(map,dirOfRangeSensors, ranges, position, orientation):
+    # splitting position for further calculations
+    pos_x = position[0]
+    pos_y = position[1]
+    pos_z = position[2]
+
+    #distance of range sensors from center of drone TODO: measure on drone and include in config file
+    distCenterDroneRangeSens = 0.1 #initial guess
+
+    # rotation matrix from orientation quaternion
+    q_w = orientation[0]
+    q_x = orientation[1]
+    q_y = orientation[2]
+    q_z = orientation[3]
+    orientation_matrix = np.array([[1 - 2 * (q_y **2 + q_z **2), 2 * (q_x * q_y - q_w * q_z), 2 * (q_x * q_z + q_y * q_w)],
+                          [2 * (q_x * q_y + q_w * q_z), 1 - 2 * (q_x **2 + q_z **2), 2 * (q_y * q_z - q_x * q_w)],
+                          [2 * (q_x * q_z - q_w * q_y), 2 * (q_y * q_z + q_w * q_x), 1 - 2 * (q_x **2 + q_y **2)]])
+
+    # range sensor direction vectors in drone frame of reference
+    row1 = np.cos(dirOfRangeSensors)
+    row2 = np.sin(dirOfRangeSensors)
+    row3 = np.zeros((1,dirOfRangeSensors.size))
+    dirVectInDroneFOR = np.vstack([row1,row2,row3])
+
+    pointCoordInDroneFOR = dirVectInDroneFOR.copy
+    #either do vector after vector --> col1, col2, ... , col8 and then hstack or for-loop TODO
+    #basic idea: pointCoordInDroneFOR[i] = ranges[i] * dirVectInDroneFOR[:,i]
+
+
+    #transformation to world frame of reference
+    pointCoordInWorldFOR = np.matmul(orientation_matrix,pointCoordInDroneFOR)
+
+    #add new points to map --> pointCoordInWorldFOR(:,i) TODO
+
+
     # TODO: Create the logic
     return map
