6cef5dcf6b84c473ea97757439cfe646e32e9e14
Adrien Sorin <adrien.sorin@tum.de>
2020-06-16 18:54:40 +0200

    Main structure of the logic for update map is laid out, some points still have to be completed.

--- local_map/src/local_map/map_logic.py
+++ local_map/src/local_map/map_logic.py
@@ -18,6 +18,36 @@ class Mapper:
         self.map = _update_map(self.map,self.dirOfRangeSensors, ranges, position, orientation)
         return self.map
 
-def _update_map(map,dirOfRangeSensors, ranges, position, orientation):
-    # TODO: Create the logic
+
+def _update_map(map, dirOfRangeSensors, ranges, position, orientation):
+    # distance of range sensors from center of drone TODO: measure on drone and include in config file
+    distCenterDroneRangeSens = 0.1  # initial guess
+
+    # rotation matrix from orientation quaternion
+    q_w = orientation[0]
+    q_x = orientation[1]
+    q_y = orientation[2]
+    q_z = orientation[3]
+    orientation_matrix = np.array(
+        [[1 - 2 * (q_y ** 2 + q_z ** 2), 2 * (q_x * q_y - q_w * q_z), 2 * (q_x * q_z + q_y * q_w)],
+         [2 * (q_x * q_y + q_w * q_z), 1 - 2 * (q_x ** 2 + q_z ** 2), 2 * (q_y * q_z - q_x * q_w)],
+         [2 * (q_x * q_z - q_w * q_y), 2 * (q_y * q_z + q_w * q_x), 1 - 2 * (q_x ** 2 + q_y ** 2)]])
+
+    # range sensor direction vectors in drone frame of reference
+    row1 = np.cos(dirOfRangeSensors)
+    row2 = np.sin(dirOfRangeSensors)
+    row3 = np.zeros((1, dirOfRangeSensors.size))
+    dirVectInDroneFOR = np.vstack([row1, row2, row3])
+
+    pointCoordInDroneFOR = dirVectInDroneFOR.copy
+    # either do vector after vector --> col1, col2, ... , col8 and then hstack or for-loop TODO
+    # basic idea: pointCoordInDroneFOR[i] = (ranges[i] + distCenterDroneRangeSens) * dirVectInDroneFOR[:,i]
+
+    # transformation to world frame of reference
+    dronePos = np.repeat(position, 8, axis=1)
+    pointCoordInWorldFOR = np.matmul(orientation_matrix,
+                                     pointCoordInDroneFOR) + dronePos
+
+    # add new points to map --> pointCoordInWorldFOR(:,i) TODO
+
     return map
