03636c4ec310e45e4feedbdd0a629b229fcd1ff7
Adrien Sorin <adrien.sorin@tum.de>
2020-07-02 19:57:28 +0200

    Started laying out some test functions for the local_map node.

--- local_map/test/map_logic_test.py
+++ local_map/test/map_logic_test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import unittest
 import sys
+import numpy as np
 
 from local_map.map_logic import Mapper
 
@@ -14,8 +15,27 @@ class TestLocalMapper(unittest.TestCase):
 
     def test_import(self):
         mapper = Mapper()
-        mapper.update([],[])
-        self.assertTrue(True,"Shoul be true if the import is working")
+        mapper.update([], [])
+        local_map = []
+        age = []
+        dirOfRangeSensors = np.array([0])
+        distCenterDroneRangeSens = np.array([0])
+        ranges = np.array([0])
+        position = np.zeros(3)
+        orientation = np.zeros(4)
+        max_readingage = 1
+        iteration = 0
+        mapper.update_map(local_map, age, dirOfRangeSensors, distCenterDroneRangeSens, ranges, position, orientation,
+                   max_readingage, iteration)
+        self.assertTrue(True, "Should be true if the import is working")
+
+    def test_rotation_matrix(self):
+        #give different values for the orientation quaternion and verify that the correct matrix is calculated
+        self.assertEqual(R_dw, #TODO) calculate the value according to the quaternion
+
+    def test_dynamic_age(self):
+        #verify that older values in the local_map variables are successfully replaced if too old
+
 
 
 
